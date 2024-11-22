31b07d232b7719d0ad534165c017b91022fbf949
vinas8 <navickaszilvinas@gmail.com>
2024-11-22 10:38:46 +0000

    Pending changes exported from your codespace

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5990;
+
+// Serve the index.html file
+app.use(express.static(path.join(__dirname, '')));
+
+// Start the server
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running at http://0.0.0.0:${PORT}`);
+});
